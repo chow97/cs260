d93474e9aea09543bce3d9d7bf791d1e787fc6bb
chow97 <kahoo.chow@gmail.com>
2021-02-04 00:21:40 +0800

    test one func

--- lab02/dlist.cpp
+++ lab02/dlist.cpp
@@ -1,3 +1,12 @@
 #include "dlist.h"
 
 //put the implmenetation of the required functions here
+void printNth(node * head,int n)
+{
+    int count = 1;
+    if (count == n)
+    {
+        cout << "Printing " << n << "th" << head->data;
+    }
+    printNth(head->next, n+1);   
+}
--- lab02/main.cpp
+++ lab02/main.cpp
@@ -8,7 +8,7 @@ int main()
     display(head);
 
     //PLEASE PUT YOUR CODE HERE to call the function assigned
-
+    printNth(head, 3);
 
     display(head);
     destroy(head);    
