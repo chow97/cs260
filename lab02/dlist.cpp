b4b40f6053e6059df05613b00d0c46fa9be09227
chow97 <kahoo.chow@gmail.com>
2021-02-04 00:45:04 +0800

    test 1.2

--- lab02/dlist.cpp
+++ lab02/dlist.cpp
@@ -11,7 +11,7 @@ void printNth(node * head,int n)
 
     if (++count == n)
     {
-        cout << "Printing " << n << "th node" << head->data;
+        cout << "Printing " << n << "th node: " << head->data;
     }    
     printNth(head->next, n);
 }
