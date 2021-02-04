7c47d1e557417a2df79646ea16087b74cf9aed99
chow97 <kahoo.chow@gmail.com>
2021-02-04 16:46:06 +0800

    final test6

--- lab02/dlist.cpp
+++ lab02/dlist.cpp
@@ -20,18 +20,6 @@ void printNth(node * head,int n)
 void insertAtN(node *& head,int n,int value)
 {
     static int count = 0;
-    //insert at nth node
-    if (n == 1)
-    {
-        node* temp = new node();
-        temp->data = value;
-        temp->previous = head->previous;
-        temp->next = head;
-        temp->previous->next = temp;
-        temp->next->previous = temp;
-        cout << "\n\nAdded "<< value << " to node number " << n + count << ": \n"; 
-        return;
-    }
     //insert at beginning
     if (head->previous == NULL && n == 1)
     {
@@ -44,6 +32,20 @@ void insertAtN(node *& head,int n,int value)
         cout << "\n\nAdded "<< value << " to first node" << ": \n";
         return;
     }
+    
+    //insert at nth node
+    if (n == 1)
+    {
+        node* temp = new node();
+        temp->data = value;
+        temp->previous = head->previous;
+        temp->next = head;
+        temp->previous->next = temp;
+        temp->next->previous = temp;
+        cout << "\n\nAdded "<< value << " to node number " << n + count << ": \n"; 
+        return;
+    }
+    
     //insert at end
     if (head->next == NULL && n != 1)
     {
