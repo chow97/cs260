c3399ea0e59077f053022d2fac429ad6ac8bb393
chow97 <kahoo.chow@gmail.com>
2021-02-04 16:47:26 +0800

    final test7

--- lab02/dlist.cpp
+++ lab02/dlist.cpp
@@ -29,10 +29,20 @@ void insertAtN(node *& head,int n,int value)
         temp->next = head;
         head->previous = temp;
         head = temp;
-        cout << "\n\nAdded "<< value << " to first node" << ": \n";
+        cout << "\n\nAdded "<< value << " to first node" << ": ";
         return;
     }
-    
+    //insert at end
+    if (head->next == NULL && n != 1)
+    {
+        node* temp = new node();
+        temp->data = value;
+        temp->previous = head;
+        temp->next = NULL;
+        head->next = temp;
+        cout << "\n\nAdded "<< value << " to last node" << ": ";
+        return;
+    }
     //insert at nth node
     if (n == 1)
     {
@@ -42,19 +52,7 @@ void insertAtN(node *& head,int n,int value)
         temp->next = head;
         temp->previous->next = temp;
         temp->next->previous = temp;
-        cout << "\n\nAdded "<< value << " to node number " << n + count << ": \n"; 
-        return;
-    }
-    
-    //insert at end
-    if (head->next == NULL && n != 1)
-    {
-        node* temp = new node();
-        temp->data = value;
-        temp->previous = head;
-        temp->next = NULL;
-        head->next = temp;
-        cout << "\n\nAdded "<< value << " to last node" << ": \n";
+        cout << "\n\nAdded "<< value << " to node number " << n + count << ": "; 
         return;
     }
     count++;
