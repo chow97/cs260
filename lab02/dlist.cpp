44e106582ed5199abc649503f305bd9963784ea0
chow97 <kahoo.chow@gmail.com>
2021-02-04 16:29:30 +0800

    final test

--- lab02/dlist.cpp
+++ lab02/dlist.cpp
@@ -25,11 +25,32 @@ void insertAtN(node *& head,int n,int value)
         temp->data = value;
         temp->previous = head->previous;
         temp->next = head;
-        temp->previous->next=temp;
-        temp->next->previous=temp;
-
+        temp->previous->next = temp;
+        temp->next->previous = temp;
         return;
     }
+    //insert at beginning
+    if (head->previous == NULL && n == 1)
+    {
+        node* temp = new node();
+        temp->data = value;
+        temp->previous = NULL;
+        temp->next = head;
+        head->previous = temp;
+        head = temp;
+        return;
+    }
+    //insert at end
+    if (head->next == NULL && n != 1)
+    {
+        node* temp = new node();
+        temp->data = value;
+        temp->previous = head;
+        temp->next = NULL;
+        head->next = temp;
+        return;
+    }
+    
     insertAtN(head->next, n - 1, value);
     
 }
