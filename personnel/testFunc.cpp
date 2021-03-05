f81b4223256f79f08bac9d09532c215a7c3ffcbf
chow97 <kahoo.chow@gmail.com>
2021-03-05 12:19:13 +0800

    testf3

--- personnel/testFunc.cpp
+++ personnel/testFunc.cpp
@@ -12,13 +12,18 @@ int main()
     h->insert(person("G123", "chow", "chow", "F123"));
     h->insert(person("G456", "lee", "kok", "F456"));
     cout << *h;*/
-    person per("MC00I238F", "Grady", "Escamilla", "FAM00I3F9O");
-    personMgr.addPerson(per);
+    person per1("MC006D018", "Lina", "Major", "FAM00CYXCW");
+    person per2("MC00I238F", "Grady", "Escamilla", "FAM00I3F9O");
+
+    personMgr.addPerson(per1);
+    personMgr.addPerson(per2);
+
     //personMgr.addPerson(person("G456", "lee", "kok", "F456"));
 
-    personMgr.findPerson("MC00I238F");
-    person * p;
-    p->print();
+    const person* perPtr = personMgr.findPerson("MC006D018");
+    perPtr->print();
+    const person* perPtr1 = personMgr.findPerson("MC00I238F");
+    perPtr1->print();
     /*
     person  * aData = NULL; 
     if (h->retrieve("G123", aData))
