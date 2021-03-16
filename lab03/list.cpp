003b27a295bf2d8ddbab8c42625a9b0480a2276b
chow97 <kahoo.chow@gmail.com>
2021-03-17 03:10:01 +0800

    tr9

--- lab03/list.cpp
+++ lab03/list.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "list.h"
-/*
+
 void reverse(node * head, node *& newHead)
 {
     if (head == NULL)
@@ -26,50 +26,10 @@ void removeLast(node *& head)
     {
         return;
     }
-    if (!head->next)
+    if (head->next->next == NULL)
     {
-        node * temp = new node;
-        temp = head;
-        head = head->next;
-        delete temp;
+        delete(head->next->next);
+        return;
     }
     removeLast(head->next);
 }
-*/
-void reverse(node *head, node *&newHead)
-{
-  if (!head)
-  {
-    return;
-  }
-
-  if (!head->next)
-  {
-    newHead = head;
-    return;
-  }
-
-  reverse(head->next, newHead);
-  head->next->next = head;
-  head->next = NULL;
-}
-
-void removeLast(node *&head)
-{
-
-  if (!head)
-  {
-    return;
-  }
-
-  if (!head->next)
-  {
-    node *temp = new node;
-    temp = head;
-    head = head->next;
-    delete temp;
-  }
-  removeLast(head->next);
-}
-
-
