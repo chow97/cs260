e0e46c38607f5d19e6a3956dd5faee5ebb1b7af7
chow97 <kahoo.chow@gmail.com>
2021-03-17 04:23:03 +0800

    test small

--- lab05/table.cpp
+++ lab05/table.cpp
@@ -1,3 +1,25 @@
 #include "table.h"
 
 //Please put the impelementation of the required functions here
+void printExtreme(node* root,bool smallest)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    if (smallest == true)
+    {
+        if (root->left == NULL)
+        {
+            cout << root->data;
+        }
+        printExtreme(root->left, true);
+        cout << root->data;
+    }
+    
+    
+}
+void copyOdd(node* root,node** newRoot)
+{
+
+}
