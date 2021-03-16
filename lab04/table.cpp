f6343098354736a4fcea5cb2b81c3d2707da85fa
chow97 <kahoo.chow@gmail.com>
2021-03-17 03:22:49 +0800

    test1

--- lab04/table.cpp
+++ lab04/table.cpp
@@ -1,3 +1,11 @@
 #include "table.h"
 
 //Please put the impelementation of the required functions here
+int countNodes(node * root)
+{
+	if(root == nullptr)
+	{
+		return 0;
+	}
+	return (root->data + countNodes(root->left) + countNodes(root->right));
+}
