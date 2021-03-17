d5433f2e40ce1829dd4e58f507f38769005e564b
chow97 <kahoo.chow@gmail.com>
2021-03-17 12:59:33 +0800

    final test

--- lab04/table.cpp
+++ lab04/table.cpp
@@ -15,30 +15,23 @@ int countNodes(node * root)
 }
 int sumLeaves(node* root)
 {
-    int sum, sumLeft, sumRight;  
-    sum = sumRight = sumLeft = 0;  
+    int sum, sumL, sumR = 0;  
       
-    //Check whether tree is empty  
     if(root == NULL) 
     {  
         return 0;  
     }  
     else 
     {  
-        //Calculate the sum of nodes present in left subtree  
         if(root->left != NULL)  
-            sumLeft = sumLeaves(root->left);  
+            sumL = sumLeaves(root->left);  
           
-        //Calculate the sum of nodes present in right subtree  
         if(root->right != NULL)  
-              sumRight = sumLeaves(root->right);  
+              sumR = sumLeaves(root->right);  
           
-        //Calculate the sum of all nodes by adding sumLeft, sumRight and root node's data  
-        sum = root->data + sumLeft + sumRight;   
+        sum = root->data + sumL + sumR;   
         return sum;  
     }      
-    
-
 }
 
     
