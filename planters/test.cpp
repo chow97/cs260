c849fb69cc2c48549ed11c441eebb1da2f11b649
chow97 <kahoo.chow@gmail.com>
2021-03-19 04:22:56 +0800

    test

--- planters/test.cpp
+++ planters/test.cpp
@@ -1,14 +1,21 @@
-#include "plantermgr.h"
+#include "box.h"
+#include "bst.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	/*
 	plantermgr p;
 
 	// insert b
 	p.plant(123, "apple");
 	p.plant(234, "pear");
+	*/
+	bst tree;
+	tree.insert(box(123, "apple"));
+	tree.insert(box(234, "pear"));
+	
 	
 }
