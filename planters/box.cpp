371cea008a77e4d248881dbc24ea2e357aeacda0
chow97 <kahoo.chow@gmail.com>
2021-03-19 11:42:41 +0800

    test *3

--- planters/box.cpp
+++ planters/box.cpp
@@ -55,3 +55,16 @@ bool operator<(const box& d1, const box& d2)
 {
 	return d1.num < d2.num;
 }
+const box& box::operator= (const box& src) {
+    if(this != &src) {
+        this->num = src.num;
+        if(this->name) {
+            delete [] this->name;
+            this->name = nullptr;
+        }
+        this->name = new char[strlen(src.name) + 1];
+        strcpy(name, src.name);
+        //cout << &name << " copied: " << name << " len: " << len << endl;
+    }
+    return *this;
+}
